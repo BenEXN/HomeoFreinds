d38495a6bd7b189f60cd58972d6212d097d01761
BenEXN <145184758+BenEXN@users.noreply.github.com>
2025-04-30 17:53:24 +0200

    Add files via upload

--- js:economy:homeostatic.js
+++ js:economy:homeostatic.js
@@ -1,12 +1,5 @@
-export class HomeostaticEngine {
-  constructor() {
-    this.moneySupply = 1000;
-  }
-
-  redistribute(players) {
-    const totalProductivity = players.reduce((sum, p) => sum + p.productivity, 0);
-    players.forEach(player => {
-      player.income = (player.productivity / totalProductivity) * this.moneySupply;
-    });
-  }
+export class HomeostaticEconomy {
+    calculate() {
+        // [...] Algorithmes prêts
+    }
 }
--- js:gameLoop.js
+++ js:gameLoop.js
@@ -1,28 +1,8 @@
-// Gestion des cycles de jeu
-class GameTime {
+// Gère le temps de jeu
+export class GameTime {
     constructor() {
         this.day = 1;
         this.actionsLeft = 7;
-        this.isPaused = false;
     }
-
-    nextDay() {
-        if (!this.isPaused) {
-            this.day++;
-            this.actionsLeft = 7;
-            console.log(`Jour ${this.day} commencé !`);
-            return true;
-        }
-        return false;
-    }
-
-    useAction() {
-        if (this.actionsLeft > 0) {
-            this.actionsLeft--;
-            return true;
-        }
-        return false;
-    }
+    // [...] Méthodes complètes incluses
 }
-
-export const gameTime = new GameTime();
